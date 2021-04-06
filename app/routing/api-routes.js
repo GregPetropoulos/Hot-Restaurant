fdeceb30f2012f6ecb0181919dc8c00fc2fdfd6f
GP <gregpetropoulos@yahoo.com>
2021-04-06 07:08:06 -0400

    Finished app, bonus work at a later date

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,7 +6,7 @@
 const tableData = require('../data/table-data');
 const waitListData = require('../data/waitinglist-data');
 
-// bring express
+// bring in app the express variable
 module.exports = function (app){
 
     // Using get function when user clicks on tables this will display tableData json object
@@ -16,5 +16,25 @@ module.exports = function (app){
 
     app.get('/api/waitlist', function(req, res){
         res.json(waitListData);
+    })
+    // post method for user to enter data to get placed at a table or waiting list.
+    // This html script tags are important to this part.
+    app.post('/api/tables', function(req, res){
+        if(tableData.length < 5){
+            tableData.push(req.body);
+            res.json(true);
+        } else{
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+// This will clear out the tables see matching ajax on tables html
+    app.post('/api/clear', function(){
+        tableData =[];
+        waitListData =[];
+
+        console.log(tableData);
+        console.log(waitListData);
+
     })
 }
